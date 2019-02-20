5fff82211dbefd00c720f1ba961dc5dd8e88121e
Programmer900 <IONIZATOR231@yandex.ru>
2019-02-20 19:47:44 +0700

    Update slides.js
    slides collection add

--- src/slides.js
+++ src/slides.js
@@ -9,34 +9,38 @@ module.exports = [
     },
     {
         id: 3,
-        image: 'http://placehold.it/150x50/g21145f'
+        image: 'http://placehold.it/150x50/c21145f'
     },
     {
         id: 4,
-        image: 'http://placehold.it/150x50/a2345f'
+        image: 'http://placehold.it/150x50/f2345f'
     },
     {
         id: 5,
-        image: 'http://placehold.it/150x50/g21145f'
+        image: 'http://placehold.it/150x50/g23445f'
     },
     {
         id: 6,
-        image: 'http://placehold.it/150x50/a22225d'
+        image: 'http://placehold.it/150x50/j22525d'
     },
     {
         id: 7,
-        image: 'http://placehold.it/150x50/g21145f'
+        image: 'http://placehold.it/150x50/g21h45f'
     },
     {
         id: 8,
-        image: 'http://placehold.it/150x50/g21145f'
+        image: 'http://placehold.it/150x50/q21122f'
     },
     {
         id: 9,
-        image: 'http://placehold.it/150x50/g21145f'
+        image: 'http://placehold.it/150x50/d41100j'
     },
     {
         id: 10,
         image: 'http://placehold.it/150x50/g21145f'
+    },
+    {
+        id: 11,
+        image: 'http://placehold.it/150x50/at2225d'
     }
 ]
